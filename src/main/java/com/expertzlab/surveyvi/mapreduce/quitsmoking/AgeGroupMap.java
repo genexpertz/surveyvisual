a44c1ee0e6386a3f33e6445ee399fd9a19549492
Avin Mathew <avin-mathew@outlook.com>
2017-09-19 15:45:23 +0530

    AgeGroup MapReduce Bug Deleted

--- src/main/java/com/expertzlab/surveyvi/mapreduce/quitsmoking/AgeGroupMap.java
+++ src/main/java/com/expertzlab/surveyvi/mapreduce/quitsmoking/AgeGroupMap.java
@@ -1,14 +1,10 @@
 package com.expertzlab.surveyvi.mapreduce.quitsmoking;
 
 import java.io.IOException;
-
-import com.sun.xml.internal.bind.CycleRecoverable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.util.StringUtils;
-
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Text;
 
 /**
  * Created by avin on 17/9/17.
@@ -19,7 +15,7 @@ public class AgeGroupMap extends Mapper<LongWritable, Text, Text,Text> {
         private Text word = new Text();
 
         @Override
-        protected void map(LongWritable key, Text value, CycleRecoverable.Context context) throws IOException, InterruptedException {
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             for(String token: StringUtils.split(value.toString(),' ')){
                 word.set(token);
                 context.write(word, new Text("1"));
