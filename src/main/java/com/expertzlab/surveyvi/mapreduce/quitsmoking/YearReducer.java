32c5e2e10987379a7852dc2eec8bdb0b60746860
Gireesh Babu <gireesh.babu@expertzlab.com>
2017-10-03 19:52:47 +0530

    Year wise smoking fixed Reducer

--- src/main/java/com/expertzlab/surveyvi/mapreduce/quitsmoking/YearReducer.java
+++ src/main/java/com/expertzlab/surveyvi/mapreduce/quitsmoking/YearReducer.java
@@ -14,9 +14,17 @@ public class YearReducer extends Reducer<Text,Text,Text,Text> {
 
     public void reduce(Text key,Iterable<Text> values,Context context) throws IOException, InterruptedException {
         int sum = 0;
+        Text yearText = new Text();
+        String year = null;
         for (Text value : values){
-            sum++;
+            String[] kevValue = value.toString().split(":");
+            if("year".equals(kevValue[0])){
+                year = kevValue[1];
+            } else if("smoking".equals(kevValue[0])) {
+                sum++;
+            }
         }
-        context.write(key,new Text("sum:1"));
+        yearText.set(year);
+        context.write(yearText,new Text(""+sum));
     }
 }
