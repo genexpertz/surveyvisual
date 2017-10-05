026625d14e260bdcdd20e7bc776e80c02a6eb99e
Avin Mathew <avin-mathew@outlook.com>
2017-10-05 21:21:34 +0530

    r script for visualization updated

---
+++ src/main/r/surveyvisual.R
@@ -0,0 +1,106 @@
+library(shiny)
+library(shinydashboard)
+
+ui <- dashboardPage(
+  dashboardHeader(title = "SurveyVisual",
+dropdownMenu(type = "notifications",
+  notificationItem(
+    text = "5 new users today",
+    icon("users")
+  )
+)
+),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Agewise Report", tabName = "overview", icon = icon("group")),
+      menuItem("Genderwise Report", tabName = "dashboard", icon = icon("bar-chart-o")),
+      menuItem("Placewise Report", tabName = "calender", icon = icon("fa fa-area-chart") ),
+      menuItem("Yearwise Report", tabName = "calender", icon = icon("calendar") )
+    )
+  ),
+   dashboardBody(
+    tabItems(
+      
+      # First tab content
+     tabItem(tabName = "overview",
+          h1("Agewise Report"),
+          
+          selectInput(inputId = "dataset",
+                      h3("Choose a year"),
+                      #label = "Choose a year:",
+                      choices = c("2010","2011","2012","2013", "2014", "2015","2016","2017"))
+          
+          ),
+     #second tab content 
+     tabItem(tabName = "dashboard",
+         h1("Genderwise Report"),
+         
+         selectInput(inputId = "dataset",
+                     h3("Choose a year"),
+                     #label = "Choose a year:",
+                     choices = c("2010","2011","2012","2013", "2014", "2015","2016","2017"))
+  ),      
+
+      # Third tab content
+      tabItem(tabName = "calender",
+        h1("Placewise Report"),
+        
+        selectInput(inputId = "dataset",
+                    h3("Choose a year"),
+                    #label = "Choose a year:",
+                    choices = c("2010","2011","2012","2013", "2014", "2015","2016","2017"))
+        
+      ),      
+  
+        # Fourth tab content
+        tabItem(tabName = "dashboard",
+                h1("Yearwise Report"),
+                
+                selectInput(inputId = "dataset",
+                            h3("Choose a year"),
+                            #label = "Choose a year:",
+                            choices = c("2010","2011","2012","2013", "2014", "2015","2016","2017"))
+              
+      )
+    ),
+tags$head(tags$style(HTML('.shiny-server-account { display: none; }'))),
+
+# The dynamically-generated user panel
+uiOutput("userpanel")
+   ),
+dashboardBody()
+)
+
+server <- function(input, output, session) {
+  output$userpanel <- renderUI({
+    # session$user is non-NULL only in authenticated sessions
+    if (!is.null(session$user)) {
+      sidebarUserPanel(
+        span("Logged in as ", session$user),
+        subtitle = a(icon("sign-out"), "Logout", href="__logout__"))
+      
+      dbAccess <- ({
+        library(rJava)
+        library(RJDBC)
+        cp <- c("/home/avin/hive/hive-1.1.0-cdh5.4.1/lib/hive-jdbc-1.1.0-cdh5.4.1-standalone.jar","/home/avin/hbase/hbase-1.0.0-cdh5.4.1/lib/hadoop-common-2.6.0-cdh5.4.1.jar");
+        .jinit(classpath=cp);
+        drv <- JDBC("org.apache.hive.jdbc.HiveDriver","/home/avin/hive/hive-1.1.0-cdh5.4.1/lib/hive-jdbc-1.1.0-cdh5.4.1-standalone.jar",identifier.quote="`");
+        conn <- dbConnect(drv, "jdbc:hive2://localhost:10000/surveyvisual","avin","");
+        result <- dbSendQuery(conn,"select * from agegroupcount");
+        d.frame <- fetch(result, n=-1);
+        print(d.frame);
+      })
+      
+      output$distPlot <- renderPlot({
+        
+        x    <- faithful$waiting  
+        bins <- 2
+
+        barplot(d.frame$agegroupcount.count,names.arg=d.frame$agegroupcount.agegroup,xlab="Age Group",ylab="Count",col="blue", main="Smoking habit chart",border="red")
+        
+      })
+    }
+  })
+}
+
+shinyApp(ui, server)
